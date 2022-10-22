8b2a8ccb23ab8fea27e9f245d3d4a6a5180b4bc0
Santiago <santiagogildl@gmail.com>
2022-10-22 22:15:27 +0200

    Carrera

--- src/Carrera.java
+++ src/Carrera.java
@@ -1,25 +1,23 @@
 class Animal{
+
+    private String nombre;
     private int prioridad;
+    private int resvalar;
+    private long caer;
+    private static int posicion;
+    public int getPosicion() {
+        return posicion;
+    }
+    public void setPosicion(int posicion) {
+        this.posicion = posicion;
+    }
 
     public int getResvalar() {
         return resvalar;
     }
-
     public void setResvalar(int resvalar) {
         this.resvalar = resvalar;
     }
-
-    public boolean isCaer() {
-        return caer;
-    }
-
-    public void setCaer(boolean caer) {
-        this.caer = caer;
-    }
-
-    private int resvalar;
-    private boolean caer;
-
     public int getPrioridad() {
         return prioridad;
     }
@@ -27,27 +25,56 @@ class Animal{
     public void setPrioridad(int prioridad) {
         this.prioridad = prioridad;
     }
+    public long getCaer() {
+        return caer;
+    }
+
+    public void setCaer(long caer) {
+        this.caer = caer;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
 }
 class Carrera {
     public static void main(String[] args) {
         Animal liebre=new Animal();
         liebre.setPrioridad(7);
         liebre.setResvalar(40);
+        liebre.setNombre("Liebre");
         Animal tortuga=new Animal();
         tortuga.setPrioridad(3);
         tortuga.setResvalar(5);
+        tortuga.setNombre("Tortuga");
         Animal caballo=new Animal();
         caballo.setPrioridad(8);
         caballo.setResvalar(45);
+        caballo.setNombre("Caballo");
         Animal perro=new Animal();
         perro.setPrioridad(5);
         perro.setResvalar(25);
+        perro.setNombre("Perro");
 
         Correr a1 = new Correr(liebre);
         Correr a2 = new Correr(tortuga);
         Correr a3 = new Correr(caballo);
         Correr a4 =new Correr(perro);
 
-
+        a1.start();
+        a2.start();
+        a3.start();
+        a4.start();
+        try {
+            a1.wait((liebre.getCaer() * 10000));
+            a2.wait((tortuga.getCaer() * 10000));
+            a3.wait((caballo.getCaer() * 10000));
+            a4.wait((perro.getCaer() * 10000));
+        }catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        }
     }
-}
--- src/Correr.java
+++ src/Correr.java
@@ -4,13 +4,17 @@ public class Correr extends Thread {
 
     b.setPrioridad(a.getPrioridad());
     b.setResvalar(a.getResvalar());
-    b.setCaer(a.isCaer());
-
+    b.setCaer(a.getCaer());
+    b.setPosicion(a.getPosicion());
+    b.setNombre(a.getNombre());
     }
 
     public void run() {
         for (int i = 0; i < 1000; i++) {
-            if((Math.random()*100 +
+            if((Math.random()*100 + b.getResvalar())>=100){
+                b.setCaer(b.getCaer()+1);
+            }
         }
+        System.out.println(b.getNombre() + " ha llegado.");
     }
 }
